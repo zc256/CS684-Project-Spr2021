719e7daa60b1c57bc27e6c17b30cb9a75f504a54
zc256 <71088874+zc256@users.noreply.github.com>
2021-04-18 18:51:06 -0400

    Add files via upload

--- CollegeRegistrar/settings.py
+++ CollegeRegistrar/settings.py
@@ -40,8 +40,19 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'pytest'
+    'pytest',
+	'django_nose',
+	'phone_field'
 ]
+
+# Use nose to run all tests
+# TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+
+# Tell nose to measure coverage on the 'foo' and 'bar' apps
+# NOSE_ARGS = [
+#     '--with-coverage',
+#     '--cover-package=foo,bar',
+# ]
 
 # AUTH_USER_MODEL = 'regsys.MyUser'
 
